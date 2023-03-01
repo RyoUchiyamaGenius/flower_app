998d99ce712a9d95777cf539429aa632c3bd1849
ryo <shominshoot.ucchi@icloud.com>
2023-03-02 00:03:15 +0900

    Try to include jquery.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
-import "custom/category_select"
 import jquery from "jquery"
 window.$ = jquery
+import "controllers"
+import "custom/category_select"
+
+alert('javascript');
