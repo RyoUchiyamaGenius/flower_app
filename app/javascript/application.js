ab88703dc0df002b607b9bca3bf94e8a96cd6e17
ryo <shominshoot.ucchi@icloud.com>
2023-03-24 22:36:26 +0900

    Add slick complete

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,4 +4,13 @@ import jquery from "jquery"
 window.$ = jquery
 import "controllers"
 import "custom/category_select"
+import slick from "slick-carousel"
 window.slick = slick
+
+$(function() {
+    alert('jquery');
+});
+
+$(document).ready(function() {
+    $('.test').slick();
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "jquery" # @3.6.4
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.4/dist/jquery.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/custom", under: "custom"
 pin "slick-carousel" # @1.8.1
