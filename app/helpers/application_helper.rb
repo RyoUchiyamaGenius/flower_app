9084637fa6cdd47242873f0375539e137e030cc3
ryo <shominshoot.ucchi@icloud.com>
2023-05-22 23:09:54 +0900

    Add situations to show page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,22 @@ module ApplicationHelper
         end
         return birthdays
     end
+
+    def get_flower_words(id)
+        flower_words = FlowerWord.where(flower_id: id)
+        words = []
+        flower_words.each do |flower_word|
+            words.append(flower_word[:word])
+        end
+        return words
+    end
+
+    def get_situations(numbers)
+        situations = []
+        sub_category_id = numbers.split(",")[1..-1].map!{|x| x.to_i}
+        sub_category_id.each do |id|
+            situations.append(SubCategory.find(id)[:name])
+        end
+        return situations
+    end
 end
