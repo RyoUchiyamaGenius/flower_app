04f6d13c488d5cc910ea1207597b293e51806569
ryo <shominshoot.ucchi@icloud.com>
2023-05-22 21:49:43 +0900

    Add birthdays to show page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def make_number_to_date(number)
+        month = number[0,2].to_i
+        day = number[2,4].to_i
+        return "#{month}月#{day}日"
+    end
+
+    def get_birthdays(numbers)
+        number_array = numbers.split(",")
+        birthdays = []
+        number_array.each do |number|
+            birthdays.append(make_number_to_date(number))
+        end
+        return birthdays
+    end
 end
