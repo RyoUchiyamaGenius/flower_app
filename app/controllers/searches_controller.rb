1cea18fa4b32366c6a962e80f1e657798dbbcb5b
ryo <shominshoot.ucchi@icloud.com>
2023-06-06 15:45:19 +0900

    Add pagination.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
 def search
     @range = params[:range]
     if @range == "花の名前"
-        @flowers = Flower.where("name like ?", "%#{params[:word]}%")
+        @flowers = Flower.where("name like ?", "%#{params[:word]}%").page(params[:page]).per(12)
         render "/searches/search_result"
     else
         @flower_words = FlowerWord.where("word like ?", "%#{params[:word]}%")
@@ -11,7 +11,8 @@ def search
         @flower_words.each do |word|
             @flowers.add(Flower.find(word.flower_id))
         end
-        @flowers.to_a
+        @flowers = @flowers.to_a
+        @flowers = Kaminari.paginate_array(@flowers).page(params[:page]).per(12)
         render "/searches/search_result"
     end
 end
@@ -21,14 +22,14 @@ end
 
 def category_search
     @sub_category = SubCategory.find(params[:sub_category])
-    @flowers = Flower.where("sub_category_id like ?", "%,#{@sub_category.id},%")
+    @flowers = Flower.where("sub_category_id like ?", "%,#{@sub_category.id},%").page(params[:page]).per(12)
     render "/searches/search_result"
 end
 
 def birthday_search
     @birthday = params["birthday(2i)"].to_i * 100 + params["birthday(3i)"].to_i
     @birthday = sprintf("%04d", @birthday)
-    @flowers = Flower.where("date like ?", "%#{@birthday}%")
+    @flowers = Flower.where("date like ?", "%#{@birthday}%").page(params[:page]).per(12)
     render "/searches/search_result"
 end
 
